65d5a2519a18e483e8192405bef7a5d385cbc213
phantomT <bobphantom@126.com>
2017-09-04 12:19:12 +0800

    更新了connect函数
    使用fread()和fwrite()函数对文本进行读写并且合并

--- 文本合并/文本合并/合并.c
+++ 文本合并/文本合并/合并.c
@@ -2,11 +2,11 @@
 
 #include <stdio.h>  
 #include <stdlib.h>  
+#include <string.h>
 
 char* get_name(void)											//不定长输入地址
 {
 	char *name, *temp;
-	char ch;
 	int j = 1;
 
 	name = (char*)malloc(sizeof(char)*(j + 1));
@@ -98,11 +98,89 @@ void combine(void)
 	fclose(fpa);
 }
 
+void connect(void)
+{
+	/*################### 打开文件1和2，并建立合并文件"Output_file.txt" ###################*/
+
+	FILE *fpa, *fpb, *fpc;
+	char *file_name_1, *file_name_2;						//文件名暂存数组  
+
+	printf("请输入文件1的相对地址：");
+	//scanf("%s", file_name_1);
+	file_name_1 = get_name();
+	printf("Openning %s\n", file_name_1);                   //状态标识  
+
+	if ((fpa = fopen(file_name_1, "r")) == NULL)            // 打开输出文件并使fpa指向此文件  
+	{
+		printf("Open failure...\n");                        // 如果打开时出错，就输出"打不开"的信息    
+		exit(0);                                            // 终止程序  
+	}
+
+	printf("\n请输入文件2的相对地址：");
+	//scanf("%s", file_name_2);
+	file_name_2 = get_name();
+	printf("Openning %s\n", file_name_2);                   //状态标识  
+
+	if ((fpb = fopen(file_name_2, "r")) == NULL)            // 打开输出文件并使fpb指向此文件  
+	{
+		printf("Open failure...\n");                        // 如果打开时出错，就输出"打不开"的信息    
+		exit(0);                                            // 终止程序  
+	}
+
+
+	if ((fpc = fopen("Output_file.txt", "w")) == NULL)      // 打开输出文件并使fpc指向此文件  
+	{
+		printf("Open failure...\n");                        // 如果打开时出错，就输出"打不开"的信息    
+		exit(0);                                            // 终止程序  
+	}
+	printf("Building Output_file.txt\n");                   //状态标识  
+
+	/*################### 将文件1和2的内容复制到文件Output_file中 ###################*/
+	int last_a, last_b;										//定义文件大小
+	char *buff;
+
+	fseek(fpa, 0, SEEK_END);								//查找第一个文件的大小
+	last_a = ftell(fpa);
+	buff = (char*)malloc(sizeof(char)*last_a);
+	if (NULL == buff)
+	{
+		exit(1);
+	}
+	rewind(fpa);											//将第一个文件指针移到开头（不可忘记）
+
+	fread(buff, 1,last_a, fpa);								//将第一个文件写入
+	fwrite(buff, 1,last_a, fpc);
+
+	free(buff);
+
+	fseek(fpc, 0, SEEK_END);								//将合并文件的指针移到最后
+	
+	fseek(fpb, 0, SEEK_END);								//查找第二个文件的大小
+	last_b = ftell(fpb);
+	buff = (char*)malloc(sizeof(char)*last_b);
+	if (NULL == buff)
+	{
+		exit(1);
+	}
+
+	rewind(fpb);											//将第二个文件指针移到开头
+	fread(buff, 1, last_b, fpb);							//将第二个文件写入
+	fwrite(buff, 1, last_b, fpc);
+	
+	free(buff);
+
+	/*################### 关闭文件Output_file和文件1、2 ###################*/
+
+	fclose(fpc);
+	fclose(fpb);
+	fclose(fpa);
+}
+
 int main(void)
 {
 	printf("Program initiating...\n");
 
-	combine();
+	connect();
 
 	system("pause");
 
